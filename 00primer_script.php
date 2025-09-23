c45a08dcbd3289317292d7890b1559eb2fb3e0f6
Rafa Lozano <rafalozano@iesgrancapitan.org>
2025-09-23 19:46:09 +0200

    Elementos del lenguaje I

---
+++ 00primer_script.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="es">
+    <head>
+        <meta charset="utf-8">
+        <title>Primer script en PHP</title>
+    </head>
+    <body>
+        <h1>Primer script en PHP</h1>
+<?php
+echo "<h3>IES Gran Capitán</h3>";
+echo "<p>Este es el primer script en PHP con DWES</p>";
+?>
+
+        <hr>
+        &copy; Rafa
+    </body>
+</html>
---
+++ ra2/01elementos_lenguaje.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="es">
+  <head>
+    <meta charset="utf-8">
+    <title>Elementos del lenguaje</title>
+  </head>
+  <body>
+    <h1>Elementos del lenguaje</h1>
+    <h2>Entrada y Salida</h2>
+    <p>La entrada de datos en PHP es con un formulario o enlace. La salida
+      se produce con la función echo, y su forma abreviada, y la función print.
+
+      Además, tenemos la función printf para salida con formato.
+    </p>
+    <h3>Función echo</h3>
+<?php
+echo "<p>La función echo emite el resultado de una expresión a la salida (del servidor
+al cliente web). Se puede usar como función o como construcción del lenguaje (sin paréntesis)</p>";
+
+echo "<p>Esto es un párrafo HTML enviado con echo</p>";
+
+
+?>
+  </body>
+</html>
