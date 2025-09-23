5850b025005f0abfaab2da5ceb7920c4bb902782
Rafa Lozano <rafalozano@iesgrancapitan.org>
2025-09-23 21:24:30 +0200

    Tipos de datos I

---
+++ ra2/02tipos_datos.php
@@ -0,0 +1,44 @@
+<!DOCTYPE html>
+<html lang="es">
+  <head>
+    <meta charset="utf-8">
+    <title>Tipos de datos</title>
+  </head>
+  <body>
+<h1>Tipos de datos</h1>
+<h2>Tipos escalares (primitivos)</h2>
+<ul>
+  <li>Booleanos</li>
+  <li>Numérico entero</li>
+  <li>Numérico en coma flotante</li>
+  <li>Cadenas de caracteres</li>
+</ul>
+
+<h2>Tipos de datos compuestos</h2>
+<ul>
+  <li>Arrays</li>
+  <li>Objetos</li>
+  <li>Callable (funciones)</li>
+  <li>Iterable</li> 
+</ul>
+
+<h2>Tipos especiales</h2>
+<ul>
+  <li>Null</li>
+  <li>Resource</li>
+</ul>
+
+<h2>Booleanos</h2>
+<p>Inicialmente las constantes true y false son valores de tipo booleano. Sin embargo
+  PHP extiende el significado de valor booleano a valor cierto y falsedad en otros 
+  tipos de datos</p>
+<ul>
+  <li>Numérico entero: 0 y -0 es false, cualquier otro es true</li>
+  <li>Númerico en coma flotante: 0.0 y -0.0 es false, cualquier otro es true</li>
+</ul>
+
+<?php
+
+?>
+</body>
+</html>
