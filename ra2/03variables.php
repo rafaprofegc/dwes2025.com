ce53270c0ecb2ae046e9a971163624c915f16791
Rafa Lozano <rafalozano@iesgrancapitan.org>
2025-09-25 16:02:11 +0200

    Variables I

---
+++ ra2/03variables.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="es">
+  <head>
+    <meta charset="utf-8">
+    <title>Tipos de datos</title>
+  </head>
+  <body>
+<h1>Variables</h1>
+<?php
+// Una variable se declara al asignarle un valor por primera vez
+// sintaxis: $variable = expresión
+
+$numero = 45;
+
+// Si utilizo una variable que no está declarada (a la que todavía
+// no le he asignado valor ), entonces es undefined.
+$resultado = $num + 4;
+echo "<p>El resultado es $resultado</p>";
+
+?>
+</body>
+</html>
