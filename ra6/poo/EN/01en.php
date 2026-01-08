f56af8f3ad84b0a8206b1c816ee18855cdef765b
Usuario <usuario@loquesea.com>
2026-01-08 16:03:12 +0100

    Extensiones I

--- ra6/poo/EN/01en.php
+++ ra6/poo/EN/01en.php
@@ -5,7 +5,6 @@ require_once("Utils/Autocarga.php");
 use EN\Utils\Autocarga;
 spl_autoload_register("\EN\Utils\Autocarga::autocarga");
 
-
 use EN\BD\conexion\ConectarBD as CBD;
 $cbd = new CBD("pepe", "localhost");
 echo "<p>{$cbd->usuario} {$cbd->servidor}";
