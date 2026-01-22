9eba822cda8eb9ce79edd601b3e7ddc5e783dc65
Usuario <usuario@loquesea.com>
2026-01-22 16:01:01 +0100

    PDO Comenzando

---
+++ ra6/pdo/01consulta.php
@@ -0,0 +1,15 @@
+<?php
+/* 
+  Parámetros de conexión a la BD
+  ------------------------------
+  Servidor: cpd.informatica.iesgrancapitan.org
+  Puerto: 9990
+  Usuario: usuario (cada uno el suyo)
+  Clave: usuario
+  BD: tiendaol
+  Juego de caracteres: utf8mb4
+*/
+require_once($_SERVER['DOCUMENT_ROOT'] . "/include/funciones.php");
+
+
+?>
