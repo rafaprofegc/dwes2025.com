280be21986fcfff6a50c172a0dc22408e06ccda4
Usuario <usuario@loquesea.com>
2026-01-29 15:59:58 +0100

    PDO I

--- ra6/pdo/01consulta.php
+++ ra6/pdo/01consulta.php
@@ -8,8 +8,44 @@
   Clave: usuario
   BD: tiendaol
   Juego de caracteres: utf8mb4
+
+  PDO
+  ---
+
+  Para acceder a una base de datos usando la API PDO:
+
+  1º Crear una conexión con la BD. Crear una instancia de la clase PDO.
+  2º Crear una consulta (con o sin parámetros). Crear una instancia de la clase
+     PDOStatement
+  3º Vincular los valores de los parámetros de la sentencia anterior, si los tiene.
+  4º Ejecutar la consulta y recibir el resultado
+  5º Acceder al conjunto de filas del resultado
+
+
 */
 require_once($_SERVER['DOCUMENT_ROOT'] . "/include/funciones.php");
+
+// Cadena DSN (Data Source Name). Esta cadena indica el SGBDR que vamos 
+// a utilizar. Además, se incluye el usuario, la clave y el esquema con
+// el que va a conectar, además de otros parámetros.
+$dsn = "mysql:host=cpd.informatica.iesgrancapitan.org;port=9990;dbname=tiendaol;charset=utf8mb4";
+$usuario = "usuario";   // Cada uno pone el suyo
+$clave = "usuario";     // Todos tenemos la misma
+$opciones = [
+  PDO::ATTR_ERRMODE             => PDO::ERRMODE_EXCEPTION,
+  PDO::ATTR_DEFAULT_FETCH_MODE  => PDO::FETCH_ASSOC,
+  PDO::ATTR_EMULATE_PREPARES    => false
+];
+
+try {
+  $pdo = new PDO($dsn, $usuario, $clave, $opciones);
+  
+}
+catch() {
+
+}
+
+
 
 
 ?>
